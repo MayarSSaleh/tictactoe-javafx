cf4352e8d1776890987a34c1d694da0938e5c380
mayar <miarsaleh@gmail.com>
2023-12-31 17:28:12 +0200

    userLogin

--- server/src/db/DataAccessLayer.java
+++ server/src/db/DataAccessLayer.java
@@ -9,6 +9,7 @@ import DTO.UsersDTO;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import org.apache.derby.jdbc.ClientDriver;
@@ -36,4 +37,37 @@ public class DataAccessLayer {
         con.close();
         return ret;
     }
+     
+     
+    public static UsersDTO isLogin( String loginEmail, String  loginPass) throws SQLException{
+        UsersDTO userLogin= new UsersDTO();
+        userLogin.setID(-1);
+        DriverManager.registerDriver(new ClientDriver());
+        Connection con =DriverManager.getConnection("jdbc:derby://localhost:1527/tictactoe","root","root");
+        
+        PreparedStatement ps = con.prepareStatement("select * from users where email = ?");
+        ps.setString(1, loginEmail); 
+        
+        ResultSet result = ps.executeQuery();
+     // ResultSet return data (string or int ) use only get string method or get int (write the column number or name)
+        if (result.next())
+        {
+            userLogin.setID(result.getInt(1));
+            userLogin.setUserName(result.getString(2));
+            userLogin.setEmail(result.getString(3));
+            userLogin.setScore(result.getInt(4));
+            userLogin.setUserPass(result.getString("USERPASS"));
+        }
+             ps.close();;
+           con.close();  
+        
+           return userLogin;
+    
+    }
+     
 }
+
+
+
+
+
